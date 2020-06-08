8b6810dcc1c5398de56ab3b3aa9fd3ba4a944453
aofarrel <aofarrel@ucsc.edu>
2020-06-08 14:23:36 -0700

    MESSY BACKUP DO NOT USE

--- Paternity Test.py
+++ Paternity Test.py
@@ -44,13 +44,13 @@
 # | 1.1 | April 14th 2020 | Ash |   made Pandas code significantly faster (n=3000 files split over 1 parent and 2 children: 340 s --> 165 s)|
 # | 1.2 | April 27th 2020 | Ash |   fixed NameError exception notice, rearranged for clarity and consistency across notebooks|
 # | 1.3 | May 19th 2020 | Ash |   better logging, indeces now zfilled, parents now sort at front in panadas code, minor clarifications|
-# 
+# | 1.4 | June 8th 2020 | Ash |   vastly improved pandas code, miscellaneous fixes|
 
 # # Setup
 
 # ## Imports
 
-# In[10]:
+# In[1]:
 
 
 import io
@@ -65,12 +65,12 @@ import logging
 
 # ## User-set variables
 
-# In[11]:
+# In[6]:
 
 
 # Don't forgot the quotation marks!
 
-SUBDIRECTORY="/thousands/" # Make sure to include slashes
+SUBDIRECTORY="/lessEdgeCases/" # Make sure to include slashes
 TABLE_NAME="table_name" #Do not include spaces or underscores
 PARENT_FILETYPE="cram"
 
@@ -88,7 +88,7 @@ INCLUDE_PARENT_EXTENSION = True
 
 # ## Environmental variables
 
-# In[12]:
+# In[3]:
 
 
 BILLING_PROJECT_ID = os.environ['GOOGLE_PROJECT']
@@ -98,7 +98,7 @@ BUCKET = os.environ["WORKSPACE_BUCKET"]
 
 # ## Call FireCloud
 
-# In[18]:
+# In[8]:
 
 
 try:
@@ -118,7 +118,7 @@ except NameError:
 # ## Display the contents of your workspace bucket
 # This step is *optional* and you might want to skip it if you have a lot of files in your bucket.
 
-# In[6]:
+# In[9]:
 
 
 get_ipython().system('gsutil ls $directory')
@@ -129,7 +129,7 @@ get_ipython().system('gsutil ls $directory')
 # ## Delete placeholder file (if you used Psuedofolder Maker)
 # Since there's now other files in the psuedofolder, you can delete the placeholder file that Folder Maker made in order to prevent it from showing up in your TSV.
 
-# In[20]:
+# In[ ]:
 
 
 get_ipython().system('gsutil rm gs${directory}placeholder')
@@ -155,13 +155,13 @@ get_ipython().system('gsutil rm gs${directory}placeholder')
 # ## Option 1: Single Child
 # This will ONLY assign a single child to each parent. Additional children will be ignored. You must write the CHILD_FILETYPE in the cell below.
 
-# In[15]:
+# In[ ]:
 
 
 CHILD_FILETYPE = "crai"
 
 
-# In[16]:
+# In[ ]:
 
 
 # Based on code written by Lon Blauvelt (UCSC)
@@ -199,7 +199,7 @@ with open(TABLE_NAME, 'w') as f:
 # ### Check output
 # This is optional and you may want to skip it if you have a lot of files.
 
-# In[17]:
+# In[ ]:
 
 
 with open(TABLE_NAME, 'r') as f:
@@ -220,43 +220,142 @@ get_ipython().system('rm $TABLE_NAME')
 
 # Unlike File Finder or Option 1, this parses the output of `gsutil ls` directly. **As a result, if your filenames contain non-ascii (ie, stuff besides A-Z, a-z, underscores, and dashes) or bizarre characters (ie, newlines) there is a chance this will not work as expected.**
 
-# In[8]:
+# In[5]:
+
+
+1) query with gsutil just to get a list of file extensions and NOTHING else
+2) do lon's code except instead of matching one child extension, match one from a list
+
+
+WAIT
+if something shares the basename of a parent, and it is not the parent itself, then it must be a child. you don't need to know what the child filetype is.
+...errr maybe you do to make sure your columns each only habve one file type
+
+current code takes 7 minutes
+
+
+# In[10]:
 
 
 logger = logging.getLogger('')
 logger.setLevel(logging.INFO)
+
+storage_client = google.cloud.storage.Client()
+google_storage_prefix = 'gs://'
+if BUCKET.startswith(google_storage_prefix):
+    bucket = BUCKET[len(google_storage_prefix):]  # clip off "gs://" prefix
+subdirectory_chopped = SUBDIRECTORY.strip("/")
+
+def progress(current_child, unique_children):
+    if current_child not in unique_children:
+        # Should never happen unless user edits code incorrectly
+        logging.error("Invalid child extension")
+        raise
+    else:
+        unique_children_list = unique_children.tolist()
+        logging.info("\t\t Processing {0} files... ({1} out of {2})".format(current_child, 
+                      unique_children_list.index(current_child)+1, 
+                      len(unique_children_list)))
+
+# Create dataframe
+df = pd.DataFrame()
+
+##Might be possible to do this in just one step. Skip anything that matches the parent file name.
+# Parse output
+logging.info("Querying your GCS bucket for extensions...")
+get_ipython().system('gsutil ls $directory > ls.txt')
+with open("ls.txt", "r") as this_file:
+    df['location'] = this_file.read().splitlines()
+
+# Split on file extension and count how many child file extensions there are
+df['FileType'] = df.location.str.rsplit('.', 1).str[-1]
+unique_children = pd.unique(df[['FileType']].values.ravel("K"))
+logging.info("{0} unique file extensions (including parent) have been found.".format(len(unique_children)))
+unique_children = unique_children[unique_children!=PARENT_FILETYPE]
+logging.info("Child file types are: %s  If this looks wrong, check your GCS directory for missing or junk files." % unique_children)
+
+
+# Option 1: Make one dataframe per child and then merge them on the parent at the end
+# Option 2: Make a dataframe linking parents and children
+# Option 3: Don't use dataframes
+# Option 2 would require iterating the parent dataframe constantly while option 1 should just do that once
+
+# It is faster to keep a list and construct a df at the end of each child then keep adding to a df
+
+logging.info("Linking parents and children...")
+list_dfs = [] # List of dataframes, one df per child extension
+for child in unique_children:
+    list_of_list_child = []
+    progress(child, unique_children)    
+    i = 0
+    for blob in storage_client.list_blobs(bucket, prefix=subdirectory_chopped):
+        if blob.name.endswith(PARENT_FILETYPE):
+            # remove PARENT_FILETYPE extension and search for basename
+            basename = blob.name[:-len(f'.{PARENT_FILETYPE}')]
+            for basename_blob in storage_client.list_blobs(bucket, prefix=basename):
+                # only add a line if there is a corresponding child file
+                if basename_blob.name.endswith(child):
+                    table_id = f'{i}'.zfill(4)
+                    #table_id = table_id.zfill(4)
+                    parent_filename = blob.name.split('/')[-1]
+                    child_filename = basename_blob.name.split('/')[-1]
+                    parent_location = f'{google_storage_prefix}{bucket}/{blob.name}'
+                    child_location  = f'{google_storage_prefix}{bucket}/{basename_blob.name}'
+                    list_child = ([table_id, parent_filename, parent_location, child_filename, child_location])
+                    i += 1
+                    list_of_list_child.append(list_child)
+    df_child = pd.DataFrame(list_of_list_child, columns=['ID','parentFile', 'parentLocation',child+'File', child+'Location'])
+    list_dfs.append(df_child)
+
+
+# In[23]:
+
+
+#Merge child dataframes on shared file name
+logging.info("Merging dataframes...")
+merged_df = pd.DataFrame()
+for df in list_dfs:
+    try:
+        merged_df = merged_df.merge(df, on=['parentFile', 'parentLocation', 'ID'])
+    except KeyError:
+        merged_df = df
+logging.info("Finished!")
+logging.info(merged_df)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
 def baseID(filename_string, child_extension):
-    PARENT_FILETYPE
-    INCLUDE_PARENT_EXTENSION
     if INCLUDE_PARENT_EXTENSION:
         fileID = filename_string.replace("."+child_extension,"")
     else:
         fileID = filename_string.replace("."+PARENT_FILETYPE+"."+child_extension,"")
     return fileID
-
-# Get location of everything and their file names
-logging.info("Querying your GCS bucket for file names...")
-get_ipython().system('gsutil ls $directory > contentlocations.txt')
-logging.info("Processing filenames...")
-get_ipython().system("cat contentlocations.txt | sed 's@.*/@@' > filenames.txt")
-
-# Import everything
-logging.info("Constructing dataframe...")
-data={}
-with open("contentlocations.txt", "r") as this_file:
-    data['filename'] = this_file.read().splitlines()
-with open("filenames.txt", "r") as that_file:
-    data['location'] = that_file.read().splitlines()
-
-# Create dataframe
-df = pd.DataFrame(data)
-
-# Split on file extension
-df['FileType'] = df.filename.str.rsplit('.', 1).str[-1]
-
-# Count file extensions to see how many child file types there are
-unique_children = pd.unique(df[['FileType']].values.ravel("K"))
-unique_children = unique_children[unique_children!=PARENT_FILETYPE]
 
 # Play with strings to get basic ID, needed to link parents and children
 logging.info("Manipulating dataframe...")
